d25546e1a808e1afc0f75d32f3adfe9deae633a8
InteractiveNinja <gabrielnadolny2@gmail.com>
2024-07-12 19:34:25 +0200

    add env reading

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+APP_URL = environ.get("APP_URL")
+APP_SEARCH_QUERY = environ.get("APP_SEARCH_QUERY")
+
+if APP_URL is None or APP_SEARCH_QUERY is None:
+    raise Exception("get gud")
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from typing import Union
-
 from fastapi import FastAPI
 from starlette.responses import Response
 from starlette.status import HTTP_204_NO_CONTENT
-
+from config import APP_URL,APP_SEARCH_QUERY
 app = FastAPI()
 
 
 @app.get("/status")
 def read_root():
     return Response(status_code=HTTP_204_NO_CONTENT)
+
+
+@app.get("/config")
+def read_root():
+    return APP_URL + " " + APP_SEARCH_QUERY
